c8b63d5d88f586555654529d8c9d24bdc49403c0
frkgui <78104494+frkgui@users.noreply.github.com>
2023-05-24 16:45:57 -0300

    changes

--- src/br/com/screenmatch/calc/timeCalculator.java
+++ src/br/com/screenmatch/calc/timeCalculator.java
@@ -1,4 +1,19 @@
 package br.com.screenmatch.calc;
+import br.com.screenmatch.models.Movie;
+import br.com.screenmatch.models.Series;
+
 
 public class timeCalculator {
+    private int totalTime;
+    public int getTotalTime() {
+        return totalTime;
+    }
+    public void include(Movie movie){
+        this.totalTime += movie.getDurationInMinute();
+    }
+
+    public void include(Series serie){
+        this.totalTime += serie.getDurationInMinute();
+    }
+
 }
